2e0a6cfc77090c3737a87890d05b51cbf1753e7e
miha2878 <88199626+miha2878@users.noreply.github.com>
2021-11-12 18:04:42 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,41 @@
+$('.wedo__drop-link').on('click', function (e){
+	e.preventDefault();
+	$(this).toggleClass("wedo__drop-link_active");
+	$(this).siblings(".wedo__drop-wrapper").slideToggle(300);
+});
+
+  $('a[href^="#"], .hero__button').click(function(){ 
+let anchor = $(this).attr('href');  
+$('html, body').animate({           
+scrollTop:  $(anchor).offset().top-190
+}, 600);                           
+});
+
+  var menu = $(".header-main");
+  $(window).scroll(function() {
+    var top = $(this).scrollTop();
+    if ( top >= 96 ) {
+    	$(".header-main").css({background: 'rgba(37, 43, 66, 1)', transition: "0.1s"});
+    }else{
+    	$(".header-main").css({background: 'inherit', transition: "0.1s"});
+    }
+  });
+
+var owlone = $(".reviews__slider").owlCarousel({
+        loop: true,
+        autoplay: false,
+        items: 1,
+        autoWidth:false,
+        nav: true,
+        navContainer: '.reviews__arrows'
+    });
+
+
+  $(document).ready(function() {
+	var top = $(this).scrollTop();
+    if ( top >= 96 ) {
+    	$(".header-main").css({background: 'rgba(37, 43, 66, 1)', transition: "0s"});
+    }else{
+    	$(".header-main").css({background: 'inherit', transition: "0s"});
+    }
+});
