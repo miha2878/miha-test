e63149f706e208fc322281b96143b633f46bad9f
miha2878 <88199626+miha2878@users.noreply.github.com>
2021-11-15 18:10:59 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,8 @@
+new WOW().init();
+
+$('a[href^="#"], .hero__button').click(function(){ 
+let anchor = $(this).attr('href');  
+$('html, body').animate({           
+scrollTop:  $(anchor).offset().top-190
+}, 600);                           
+});
