6027bb86eed1cb385131d0035b35fc3f42823568
miha2878 <88199626+miha2878@users.noreply.github.com>
2021-11-22 12:44:42 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,37 @@
+new WOW().init();
+
+$('.wedo__drop-link').on('click', function (e){
+	e.preventDefault();
+	$(this).toggleClass("wedo__drop-link_active");
+	$(this).siblings(".wedo__drop-wrapper").slideToggle(300);
+});
+
+  $('a[href^="#"], .hero__button').click(function(){ 
+let anchor = $(this).attr('href');  
+$('html, body').animate({           
+scrollTop:  $(anchor).offset().top-200
+}, 600);                           
+});
+
+  var menu = $(".header-main");
+  $(window).scroll(function() {
+    var top = $(this).scrollTop();
+    if ( top >= 96 ) {
+    	$(".header-main").css({background: '#202638', transition: "0.1s"});
+    }else{
+    	$(".header-main").css({background: 'inherit', transition: "0.1s"});
+    }
+  });
+
+
+
+
+
+  $(document).ready(function() {
+	var top = $(this).scrollTop();
+    if ( top >= 96 ) {
+    	$(".header-main").css({background: '#202638', transition: "0s"});
+    }else{
+    	$(".header-main").css({background: 'inherit', transition: "0s"});
+    }
+});
