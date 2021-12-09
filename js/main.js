f2ddc5c62deeec2aea112e075dec1b5cde3725c4
miha2878 <88199626+miha2878@users.noreply.github.com>
2021-12-09 17:15:46 +0300

    Delete js directory

--- js/main.js
+++
@@ -1,9 +0,0 @@
-new WOW().init();
-
-  $('a[href^="#"]').click(function(){ 
-let anchor = $(this).attr('href');  
-$('html, body').animate({           
-scrollTop:  $(anchor).offset().top-200
-}, 600);                           
-});
-
